623a98f686e54152eb8dd4bcf517a99abe099e80
Maksim Moryakov <maksim.moryakov44@yandex.ru>
2023-02-25 01:24:02 +0300

    Попытка сделать счетчик страниц

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,29 +2,37 @@ from .models import Group, Post
 
 from django.shortcuts import render, get_object_or_404
 
+from django.core.paginator import Paginator
+
 POSTS_ON_PAGE: int = 10
 
 
 def index(request):
-    template = 'posts/index.html'
     title = 'Последние обновления на сайте'
     posts = Post.objects.all()[:POSTS_ON_PAGE]
+    paginator = Paginator(posts, POSTS_ON_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'title': title,
         'posts': posts,
+        'page_obj': page_obj,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    template = 'posts/group_list.html'
     title = 'Здесь будет информация о группах проекта Yatube'
     posts = group.posts.all()[:POSTS_ON_PAGE]
+    paginator = Paginator(posts, POSTS_ON_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'group': group,
         'title': title,
         'posts': posts,
+        'page_obj': page_obj,
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
 
