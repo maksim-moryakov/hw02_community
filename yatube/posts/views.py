54ac7a9a15c7abad972e4c58bc7ed446a65e2c3a
Maksim Moryakov <maksim.moryakov44@yandex.ru>
2023-02-25 01:56:42 +0300

    Доделал страницы

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -9,13 +9,12 @@ POSTS_ON_PAGE: int = 10
 
 def index(request):
     title = 'Последние обновления на сайте'
-    posts = Post.objects.all()[:POSTS_ON_PAGE]
-    paginator = Paginator(posts, POSTS_ON_PAGE)
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, POSTS_ON_PAGE)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
         'title': title,
-        'posts': posts,
         'page_obj': page_obj,
     }
     return render(request, 'posts/index.html', context)
@@ -24,14 +23,13 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     title = 'Здесь будет информация о группах проекта Yatube'
-    posts = group.posts.all()[:POSTS_ON_PAGE]
-    paginator = Paginator(posts, POSTS_ON_PAGE)
+    post_list = group.posts.all()
+    paginator = Paginator(post_list, POSTS_ON_PAGE)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
         'group': group,
         'title': title,
-        'posts': posts,
         'page_obj': page_obj,
     }
     return render(request, 'posts/group_list.html', context)
