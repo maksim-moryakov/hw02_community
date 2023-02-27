ce3421444a498f50b4c769141a563db03a76dca0
Maksim Moryakov <maksim.moryakov44@yandex.ru>
2023-02-27 23:04:12 +0300

    Создали страницу пользователя и страницу поста

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,4 +1,4 @@
-from .models import Group, Post
+from .models import Group, Post, User
 
 from django.shortcuts import render, get_object_or_404
 
@@ -7,12 +7,16 @@ from django.core.paginator import Paginator
 POSTS_ON_PAGE: int = 10
 
 
+def paginator_object(request, post_list):
+    paginator = Paginator(post_list, POSTS_ON_PAGE)
+    page_number = request.GET.get('page')
+    return paginator.get_page(page_number)
+
+
 def index(request):
     title = 'Последние обновления на сайте'
     post_list = Post.objects.all()
-    paginator = Paginator(post_list, POSTS_ON_PAGE)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    page_obj = paginator_object(request, post_list)
     context = {
         'title': title,
         'page_obj': page_obj,
@@ -24,9 +28,7 @@ def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     title = 'Здесь будет информация о группах проекта Yatube'
     post_list = group.posts.all()
-    paginator = Paginator(post_list, POSTS_ON_PAGE)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    page_obj = paginator_object(request, post_list)
     context = {
         'group': group,
         'title': title,
@@ -34,3 +36,24 @@ def group_posts(request, slug):
     }
     return render(request, 'posts/group_list.html', context)
 
+
+def profile(request, username):
+    user_author = get_object_or_404(User, username=username)
+    post_list = user_author.posts.all()
+    page_obj = paginator_object(request, post_list)
+    context = {
+        'page_obj': page_obj,
+        'user_author': user_author
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    post_number = Post.objects.select_related('author').filter(
+        author=post.author).count()
+    context = {
+        'post': post,
+        'post_number': post_number
+    }
+    return render(request, 'posts/post_detail.html', context)
