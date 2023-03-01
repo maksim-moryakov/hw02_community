27535b8bbc2eb96786bd9b1422339de01e5aecf4
Maksim Moryakov <maksim.moryakov44@yandex.ru>
2023-03-01 17:47:43 +0300

    Доделал страницу создания поста

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -54,8 +54,7 @@ def profile(request, username):
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    post_number = Post.objects.select_related('author').filter(
-        author=post.author).count()
+    post_number = post.author.posts.count()
     context = {
         'post': post,
         'post_number': post_number
