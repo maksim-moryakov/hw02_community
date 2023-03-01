d798deff1b2bd38910de436f08742a2fce71ea65
Maksim Moryakov <maksim.moryakov44@yandex.ru>
2023-03-02 00:01:45 +0300

    Муки с Логаут

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LogoutView, LoginView 
 
 from django.urls import path
 
@@ -8,14 +8,8 @@ app_name = 'users'
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
-    path(
-        'logout/',
-        LoginView.as_view(template_name='users/logged_out.html'),
-        name='logout'
-    ),
-    path(
-        'login/',
-        LoginView.as_view(template_name='users/login.html'),
+    path('logout/', LogoutView.as_view(template_name='users/logged_out.html'), name='logout'),
+    path('login/', LoginView.as_view(template_name='users/login.html'),
         name='login'
     ),
 ]
