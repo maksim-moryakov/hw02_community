b4a2edf402c0d9fdec4f82e65bf9e0f1666ac57c
Maksim Moryakov <maksim.moryakov44@yandex.ru>
2023-03-02 23:03:19 +0300

    Доработка смена пароля и правки ревью

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -7,3 +7,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('group', 'text')
+        help_text = {
+            'text': '*Введите текст поста',
+            'group': 'Выберите группу, к которой будет относиться пост'
+        }
--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,5 +1,9 @@
-from django.contrib.auth.views import LogoutView, LoginView 
-
+from django.contrib.auth.views import (
+    LoginView, LogoutView, PasswordChangeView,
+    PasswordResetView, PasswordChangeDoneView,
+    PasswordResetDoneView, PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
 from django.urls import path
 
 from . import views
@@ -12,4 +16,28 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'),
         name='login'
     ),
+    path('password_change/',
+         PasswordChangeView.as_view(
+         template_name='users/password_change_form.html'),
+         name='password_change_form'),
+    path('password_change_done/',
+         PasswordChangeDoneView.as_view(
+         template_name='users/password_change_done.html'),
+         name='password_change_done'),
+    path('password_reset/',
+         PasswordResetView.as_view(
+         template_name='users/password_reset_form.html'),
+         name='password_reset_form'),
+    path('password_reset/done/',
+         PasswordResetDoneView.as_view(
+         template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(
+         template_name='users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('reset/done/',
+         PasswordResetCompleteView.as_view(
+         template_name='users/password_reset_complete.html'),
+         name='password_reset_compete')
 ]
